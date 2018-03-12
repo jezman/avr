e46c766190ade18d97756556acf53d0a193f8866
jezman <dev.jezman@gmail.com>
2018-03-12 16:48:22 +0300

    timer1

--- src/main.c
+++ src/main.c
@@ -1,54 +1,40 @@
 #include <avr/io.h>
-#include <util/delay.h>
+#include <avr/interrupt.h>
 
-#define F_CPU 16000000L
+#define F_CPU 16000000UL
 
-void setChar(unsigned char seg)
-{
-  switch (seg)
-  {
-  case 1: PORTD = 0b11111001; break;
-  case 2: PORTD = 0b10100100; break;
-  case 3: PORTD = 0b10110000; break;
-  case 4: PORTD = 0b10011001; break;
-  case 5: PORTD = 0b10010010; break;
-  case 6: PORTD = 0b10000010; break;
-  case 7: PORTD = 0b11111000; break;
-  case 8: PORTD = 0b10000000; break;
-  case 9: PORTD = 0b10010000; break;
-  case 0: PORTD = 0b11000000; break;
-  }
+uint8_t i;
+
+void setChar (uint8_t seg) {
+	switch(seg) {
+	case 1: PORTD = 0b11111001; break;
+	case 2: PORTD = 0b10100100; break;
+	case 3: PORTD = 0b10110000; break;
+	case 4: PORTD = 0b10011001; break;
+	case 5: PORTD = 0b10010010; break;
+	case 6: PORTD = 0b10000010; break;
+	case 7: PORTD = 0b11111000; break;
+	case 8: PORTD = 0b10000000; break;
+	case 9: PORTD = 0b10010000; break;
+	case 0: PORTD = 0b11000000; break;
+	}
 }
 
-int main(void)
-{
-  unsigned char i;
-  unsigned char buttonCount = 0;
-  unsigned char buttonState = 0;
+int main(void) {
+  OCR1A = 0x30D3; 
+  TCCR1B |= (1 << WGM12); // Mode 4, CTC on OCR1A
+  TIMSK1 |= (1 << OCIE1A); //Set interrupt on compare match
+  TCCR1B |= (1 << CS12) | (1 << CS10); // set prescaler to 1024 and start the timer
+  sei();         //enable interrupts
 
-  DDRB &= ~_BV(PB5); // set button pin input
-  PORTB |= _BV(PD5); // set pull-up
-  DDRD |= 0xFF; // set 7segment led port output
+  DDRD = 0xFF;
+  PORTD = 0b11111111;
 
-  while (1) {
-    for (i = 0; i <= 9; i++) {
-      // check button
-      while (1) {
-        if (!(PIND & _BV(PD5))) {
-          if (buttonCount < 5) {
-            buttonCount++;
-          } else {
-            i = 0;
-            buttonState = 1;
-          }
-        } else
-          (buttonCount > 0) ? buttonCount-- : (buttonState = 1);
-      }
+  while (1) {}
+}
 
-      setChar(i);
-      _delay_ms(500);
-
-      buttonState = 0;
-    }
-  }
+ISR (TIMER1_COMPA_vect) {
+  if (i > 9) i = 0;
+  setChar(i);
+  i++;
 }
